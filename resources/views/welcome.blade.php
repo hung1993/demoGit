d2e2a4080e7b386b37ecdc4e2c75be46fd75e150
hung1993 <darius331993@gmail.com>
2018-01-11 19:25:40 +0700

    branch01 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home de mo</a>
+                        <a href="{{ url('/home') }}">Home demo Branch</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
